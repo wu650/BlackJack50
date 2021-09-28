d27726f8e060520ec63dc9969d2a5ba0bb921f4b
wu650 <williamunterkofler@gmail.com>
2021-09-27 23:37:46 -0400

    Added and changed gameplay mechanics user and dealer
    *Updated dealer logic and hit or stick logic to reflect the improved hand value function
    *Added dealStartingHands function to give all players 2 cards
    *Added check for natural blackjack at the start of each round
    *Natural blackjacks are reflected in the hand tally scoreboard

--- src/com/blackjacksp/backend/GamePlay.java
+++ src/com/blackjacksp/backend/GamePlay.java
@@ -39,6 +39,9 @@ public class GamePlay {
         int numHands = scan.nextInt();
         this.numHands = numHands;
 
+        // Add dealer to players list (will always be index 0)
+        playerList.add(new Player("Dealer", true));
+
         // Get user input for their player name
         System.out.println("What is your name?");
         String playerName = scan.nextLine();
@@ -46,38 +49,84 @@ public class GamePlay {
         // Add user to players list
         playerList.add(new Player(playerName, false));
 
-        // Add dealer to players list
-        playerList.add(new Player("Dealer", true));
-
-        this.initializeHandTally();
+        initializeHandTally();
 
         scan.close();
+    }
+
+    public void playHand() {
+        // TODO
+    }
+
+    // Will disperse the initial two cards
+    public void dealStartingHands() {
+        for (Player player : playerList) {
+
+            // Always deals to non-dealers first
+            if (!player.isDealer()) {
+                for (int i = 0; i < 2; i++) {
+                    cardPool.dealCard(player);
+                }
+            }
+            else if (player.isDealer()) {
+                for (int i = 0; i < 2; i++) {
+                    cardPool.dealCard(player);
+                }
+            }
+        }
+    }
+
+    // Check if dealer or player(s) has natural blackjack
+    public boolean naturalBlackjack() {
+        if (playerList.get(0).handValue() == 21 && playerList.get(1).handValue() == 21) {
+            handTally.put("Tied", handTally.get("Tied") + 1);
+            return true;
+        }
+        else if (playerList.get(0).handValue() == 21) {
+            handTally.put("Lost", handTally.get("Lost") + 1);
+            return true;
+        }
+        else if (playerList.get(1).handValue() == 21) {
+            handTally.put("Won", handTally.get("Won") + 1);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     // Main gameplay for the user each round (hit or stick)
     public void hitOrStickCycle(Player player) {
         Scanner scan = new Scanner(System.in);
         while (!player.isSticking()) {
-            System.out.printf("Would you like to hit or stick?");
+            System.out.printf("Would you like to hit or stick?\n");
             String choice = scan.nextLine();
             if (choice.trim().equalsIgnoreCase("Stick") || choice.trim().equalsIgnoreCase("S")) {
                 player.setSticking(true);
             }
             else if (choice.trim().equalsIgnoreCase("Hit") || choice.trim().equalsIgnoreCase("H")) {
                 cardPool.dealCard(player);
+
                 // Tells the player what card they were dealt
-                System.out.printf("You were dealt a %s of %ss", player.getHand().get(player.getHand().size() - 1).getTitle(), player.getHand().get(player.getHand().size() - 1).getSuit());
-                if (player.handValue() > 21) {
+                System.out.printf("You were dealt a %s of %ss\n", player.getHand().get(player.getHand().size() - 1).getTitle(), player.getHand().get(player.getHand().size() - 1).getSuit());
+
+                // Player will automatically stick if hand totals 21
+                if (player.handValue() == 21) {
+                    System.out.println("You have BlackJack! It is now the dealer's turn");
+                    player.setSticking(true);
+                }
+                else if (player.handValue() > 21) {
                     System.out.println("You exceeded 21! Hand over");
                     break;
                 }
                 else {
-                    System.out.printf("Your hand now consists of:");
+                    System.out.printf("Your hand now consists of:\n");
                     for (Card card : player.getHand()) {
                         System.out.printf("%s of %ss", card.getTitle(), card.getSuit());
                     }
                 }
             }
+            // Repeats loop for invalid entries
             else {
                 System.out.println("Please enter Stick or Hit");
             }
